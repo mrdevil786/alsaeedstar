c20cdea5c9beeb0f62c66eba785b0e0265720883
mrdevil786 <suhailsaeedme@gmail.com>
2024-09-15 16:03:30 +0530

    Initial COmmit

--- routes/site.php
+++ routes/site.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\Frontend\BasicController;
 use App\Http\Controllers\Frontend\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/',[HomeController::class,'home'])->name('frontend.home');
+Route::get('/',[HomeController::class,'index'])->name('frontend.home');
 Route::get('/about',[BasicController::class,'about'])->name('frontend.about');
 Route::get('/service',[BasicController::class,'service'])->name('frontend.service');
 Route::get('/contact',[BasicController::class,'contact'])->name('frontend.contact');
