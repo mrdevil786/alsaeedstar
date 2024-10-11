c9febdd31c712aa398be65f758e0a3a07661b88c
Md Kashif <kashifmd607@gmail.com>
2024-10-11 15:39:13 +0530

    added testimonial

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
 use App\Models\Team;
+use App\Models\Testimonial;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -11,6 +12,7 @@ class HomeController extends Controller
     public function index()
     {
         $teams = Team::where('status', 'active')->get();
-        return view('site.home', compact('teams'));
+        $testimonials = Testimonial::where('status', 'active')->get();
+        return view('site.home', compact('teams','testimonials'));
     }
 }
--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -429,41 +429,34 @@
                 <img class="img-fluid w-100 h-100" src="{{ asset('frontend/img/testimonial.jpg') }}">
             </div>
             <div class="col-xl-8 col-lg-7 col-md-12">
+                @if ($testimonials->isEmpty())
+                <div class="col-12 text-center">
+                    <p>No testimonial found.</p>
+                </div>
+             @else
+                @foreach ($testimonials as $testimonial)
                 <div class="testimonial bg-light">
                     <div class="owl-carousel testimonial-carousel">
                         <div class="row gx-4 align-items-center">
                             <div class="col-xl-4 col-lg-5 col-md-5">
+
                                 <img class="img-fluid w-100 h-100 bg-light p-lg-3 mb-4 mb-md-0"
-                                    src="{{ asset('frontend/img/testimonial-1.jpg') }}" alt="">
+                                    src="{{ asset($testimonial->avatar) }}" alt="">
                             </div>
                             <div class="col-xl-8 col-lg-7 col-md-7">
-                                <h4 class="text-uppercase mb-0">Client Name</h4>
-                                <p>Profession</p>
-                                <p class="fs-5 mb-0"><i class="fa fa-2x fa-quote-left text-primary me-2"></i> Dolores
-                                    sed duo
-                                    clita tempor justo dolor et stet lorem kasd labore dolore lorem ipsum. At lorem
-                                    lorem magna ut labore et tempor diam tempor erat. Erat dolor rebum sit
-                                    ipsum.</p>
+                                <h4 class="text-uppercase mb-0">{{$testimonial->name}}</h4>
+                                <p>{{$testimonial->title}}</p>
+                                <p class="fs-5 mb-0"><i class="fa fa-2x fa-quote-left text-primary me-2"></i> {{$testimonial->description}}</p>
                             </div>
                         </div>
-                        <div class="row gx-4 align-items-center">
-                            <div class="col-xl-4 col-lg-5 col-md-5">
-                                <img class="img-fluid w-100 h-100 bg-light p-lg-3 mb-4 mb-md-0"
-                                    src="{{ asset('frontend/img/testimonial-2.jpg') }}" alt="">
-                            </div>
-                            <div class="col-xl-8 col-lg-7 col-md-7">
-                                <h4 class="text-uppercase mb-0">Client Name</h4>
-                                <p>Profession</p>
-                                <p class="fs-5 mb-0"><i class="fa fa-2x fa-quote-left text-primary me-2"></i> Dolores
-                                    sed duo
-                                    clita tempor justo dolor et stet lorem kasd labore dolore lorem ipsum. At lorem
-                                    lorem magna ut labore et tempor diam tempor erat. Erat dolor rebum sit
-                                    ipsum.</p>
-                            </div>
+
                         </div>
                     </div>
                 </div>
+                @endforeach
+           @endif
             </div>
+
         </div>
     </div>
     <!-- Testimonial End -->
